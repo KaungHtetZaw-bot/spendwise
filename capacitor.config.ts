22be46f52b9d5c672b936bc7e280037b2b496185
KaungHtetZaw <Neo@KaungHtetZaw-MacBook-Pro-M1.local>
2026-04-18 17:25:24 +0630

    capacitor setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.spendwise.app',
+  appName: 'spendwise',
+  webDir: 'dist'
+};
+
+export default config;
